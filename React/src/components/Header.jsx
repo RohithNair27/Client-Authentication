a5895cf198aa2b4ab7487ac7f0c2c51e44b128ef
rohith <rohithnair2711@gmail.com>
2025-03-01 12:09:02 +0530

    UI fix: Update the menu functionality and UI

--- React/src/components/Header.jsx
+++ React/src/components/Header.jsx
@@ -3,43 +3,121 @@ import HeaderStyle from "./Header.module.css";
 import { NavLink } from "react-router";
 import { Button } from "@chakra-ui/react";
 import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from "./ui/menu";
+import { useAuth } from "../hooks/useAuth";
 function Header() {
+  let { isLoggedIn } = useAuth();
   return (
     <header>
       <h1>Authentication</h1>
-      <MenuRoot>
-        <MenuTrigger asChild>
-          <Button variant="outline" size="sm">
-            Open
-          </Button>
-        </MenuTrigger>
-        <MenuContent>
-          <MenuItem value="new-txt-a">
-            <NavLink to="/">Login</NavLink>
-          </MenuItem>
-          <MenuItem value="new-file-a">
-            <NavLink to="/signup">Signup</NavLink>
-          </MenuItem>
-          <MenuItem value="new-win-a">
-            <NavLink to="/commonpageone">Common Page One</NavLink>
-          </MenuItem>
-          <MenuItem value="open-file-a">
-            <NavLink to="/commonpageone/commonpagetwo">Common Page Two</NavLink>
-          </MenuItem>
-          <MenuItem value="export-a">
-            <NavLink to="/adminpageone">Admin Page One</NavLink>
-          </MenuItem>
-          <MenuItem value="export-b">
-            <NavLink to="/adminpageone/adminpagetwo">Admin Page Two</NavLink>
-          </MenuItem>
-          <MenuItem value="export-c">
-            <NavLink to="/userpageone">User Page One</NavLink>
-          </MenuItem>
-          <MenuItem value="export-d">
-            <NavLink to="/userpageone/userpagetwo">User Page Two</NavLink>
-          </MenuItem>
-        </MenuContent>
-      </MenuRoot>
+      {isLoggedIn ? (
+        <MenuRoot>
+          <MenuTrigger asChild>
+            <Button variant="outline" size="sm">
+              Open
+            </Button>
+          </MenuTrigger>
+          <MenuContent>
+            <MenuItem>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${HeaderStyle.nav_btn} ${HeaderStyle.active}`
+                    : HeaderStyle.nav_btn
+                }
+              >
+                Login
+              </NavLink>
+            </MenuItem>
+            <MenuItem value="new-file-a">
+              <NavLink
+                to="/signup"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${HeaderStyle.nav_btn} ${HeaderStyle.active}`
+                    : HeaderStyle.nav_btn
+                }
+              >
+                Signup
+              </NavLink>
+            </MenuItem>
+            <MenuItem value="new-win-a">
+              <NavLink
+                to="/commonpageone"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${HeaderStyle.nav_btn} ${HeaderStyle.active}`
+                    : HeaderStyle.nav_btn
+                }
+              >
+                Common Page One
+              </NavLink>
+            </MenuItem>
+            <MenuItem value="open-file-a">
+              <NavLink
+                to="/commonpageone/commonpagetwo"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${HeaderStyle.nav_btn} ${HeaderStyle.active}`
+                    : HeaderStyle.nav_btn
+                }
+              >
+                Common Page Two
+              </NavLink>
+            </MenuItem>
+            <MenuItem value="export-a">
+              <NavLink
+                to="/adminpageone"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${HeaderStyle.nav_btn} ${HeaderStyle.active}`
+                    : HeaderStyle.nav_btn
+                }
+              >
+                Admin Page One
+              </NavLink>
+            </MenuItem>
+            <MenuItem value="export-b">
+              <NavLink
+                to="/adminpageone/adminpagetwo"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${HeaderStyle.nav_btn} ${HeaderStyle.active}`
+                    : HeaderStyle.nav_btn
+                }
+              >
+                Admin Page Two
+              </NavLink>
+            </MenuItem>
+            <MenuItem value="export-c">
+              <NavLink
+                to="/userpageone"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${HeaderStyle.nav_btn} ${HeaderStyle.active}`
+                    : HeaderStyle.nav_btn
+                }
+              >
+                User Page One
+              </NavLink>
+            </MenuItem>
+            <MenuItem value="export-d">
+              <NavLink
+                to="/userpageone/userpagetwo"
+                className={({ isActive }) =>
+                  isActive
+                    ? `${HeaderStyle.nav_btn} ${HeaderStyle.active}`
+                    : HeaderStyle.nav_btn
+                }
+              >
+                User Page Two
+              </NavLink>
+            </MenuItem>
+          </MenuContent>
+        </MenuRoot>
+      ) : (
+        <></>
+      )}
     </header>
   );
 }
